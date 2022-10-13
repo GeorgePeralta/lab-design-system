865a213ba5181f7bb0140d13875b7ef2760a9d49
George Peralta <george.dias@meta.com.br>
2022-10-13 12:24:00 -0300

    updating text color

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -10,7 +10,7 @@ export interface TextInputRootProps {
 
  function TextInputRoot(props: TextInputRootProps) {
   return (
-    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full  focus-within:ring-2 ring-cyan-300">
+    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full  focus-within:ring-2 ring-orange-500">
       {props.children}
     </div>  
   )
@@ -24,7 +24,7 @@ export interface TextInputIconProps {
 
 function TextInputIcon(props: TextInputIconProps) {
   return (
-    <Slot className="w-6 h-6 text-gray-400">
+    <Slot className="w-6 h-6 text-cream-300">
       {props.children}
     </Slot>
   )
@@ -37,7 +37,7 @@ export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElemen
 function TextInputInput(props: TextInputInputProps) {
 
   return (
-      <input className="bg-transparent flex-1 outline-none text-gray-100 text-xs placeholder:text-gray-400"
+      <input className="bg-transparent flex-1 outline-none text-cream-300 text-xs placeholder:text-gray-400"
       {...props}
       />      
   )
