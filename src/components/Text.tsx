3133e14b8e68b3904fb188a0d6ba3cab4975353d
George Peralta <george.dias@meta.com.br>
2022-10-13 12:10:47 -0300

    updating style guide

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -12,7 +12,7 @@ export function Text({ size = 'md', children, asChild }: TextProps) {
   const Comp = asChild ? Slot : 'span';
 
   return (
-    <Comp className={clsx("text-gray-100 font-sans", {
+    <Comp className={clsx("text-cream-300 font-sans", {
       'text-xs': size === 'sm',
       'text-sm': size === 'md',
       'text-md': size === 'lg',
