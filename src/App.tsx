906845f57a0ebf4d24439a0958c88512a8170666
George Peralta <george.dias@meta.com.br>
2022-10-13 21:10:20 -0300

    import SignIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
+import { SignIn } from './pages/SignIn'
+
 import './styles/global.css'
 
 export function App() {
   return (
-    <>
-    <h1 className="font-bold text-5xl text-violet-500">Hello word</h1>
-
-    <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
-      Enviar
-    </button>
-    </>
+    <SignIn />
   )
 }
